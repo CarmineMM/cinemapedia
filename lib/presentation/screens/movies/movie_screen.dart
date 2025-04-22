8d5109d67ed12bce8fb388ea1cc80c115bb10f67
Carmine Maggio <c.maggio@tiendasdaka.com>
2025-04-22 15:42:12 -0400

    feat: movie provider impl

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   static const routePath = 'movie/:id';
   static const routeName = 'movie.find';
 
@@ -9,7 +9,17 @@ class MovieScreen extends StatelessWidget {
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Movie $movieId')));
+    return Scaffold(appBar: AppBar(title: Text('Movie ${widget.movieId}')));
   }
 }
